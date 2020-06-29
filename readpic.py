fadf9fd75678837f1b6c48f996ef0ba804694e57
jeffabc1997 <55129114+jeffabc1997@users.noreply.github.com>
2020-06-29 17:05:34 +0800

    readpic2

--- readpic.py
+++ readpic.py
@@ -45,3 +45,35 @@ def creating_training_data():
             
 creating_training_data()
 print(len(training_data))
+
+
+#Jeff Edited
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+
+X = pickle.load(open("X.pickle", "rb")) # X, y 要修改
+y = pickle.load(open("y.pickle", "rb"))
+
+X = X/255.0
+
+model = Sequential()
+
+model.add( Conv2D(64, (3,3), input_shape = X.shape[1:]) )
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Conv2D(64), (3,3))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+
+model.compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = ['accuracy'])
+
+model.fit(X, y, batch_size, epochs = 5, validation_split = 0.1)
